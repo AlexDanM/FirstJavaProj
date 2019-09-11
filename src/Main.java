ab2f27c3afd12c5f56f5b0d7f084863bd3cb8672
Alex <alexdanmoldovan1@gmail.com>
2019-09-11 20:50:50 +0300

    test

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        // tipuri de date
+
+        String model = "golf";
+        String brand = "vw";
+        int year = 2012;
+        int mileage = 28172626;
+        String color = "albastru";
+
+        System.out.println("Masina mea este un " + brand + " " + model + ", fiind fabricata in anul " + year + " avand un kilometraj de " + mileage + "km " + "si culoarea " + color);
+
+    }
+}
